c45c0a1f5fe62a9b294e691ef848961cec987747
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-03-25 11:25:17 +0000

    Add help_message unit test

---
+++ test/unit/merge_repo/help_message.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  help_message.py
+
+    Description:  Unit testing of help_message in merge_repo.py.
+
+    Usage:
+        test/unit/merge_repo/help_message.py
+
+    Arguments:
+        None
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import sys
+import os
+import unittest
+
+# Third-party
+
+# Local
+sys.path.append(os.getcwd())
+import merge_repo
+import lib.gen_libs as gen_libs
+import version
+
+# Version Information
+__version__ = version.__version__
+
+
+class UnitTest(unittest.TestCase):
+
+    """Class:  UnitTest
+
+    Description:  Class which is a representation of a unit testing.
+
+    Super-Class:  unittest.TestCase
+
+    Sub-Classes:  None
+
+    Methods:
+        test_help_message -> Test with no arguments.
+
+    """
+
+    def test_help_message(self):
+
+        """Function:  test_help_message
+
+        Description:  Test help_message function.
+
+        Arguments:
+            None
+
+        """
+
+        with gen_libs.no_std_out():
+            self.assertFalse(merge_repo.help_message())
+
+
+if __name__ == "__main__":
+    unittest.main()
