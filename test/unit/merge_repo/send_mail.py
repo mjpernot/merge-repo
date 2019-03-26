2bae582ecdaa24f02f7c2ff6af2895e2198d63a7
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-03-26 18:51:07 +0000

    Update test/unit/merge_repo/send_mail.py

--- test/unit/merge_repo/send_mail.py
+++ test/unit/merge_repo/send_mail.py
@@ -90,13 +90,14 @@ class UnitTest(unittest.TestCase):
 
                 """
 
-                self.url="git@gitlab.code.dicelab.net:JAC-IDM/"
-                self.work_dir="/home/mark.j.pernot/merge/work_dir"
-                self.err_dir="/home/mark.j.pernot/merge/error_dir"
-                self.archive_dir="/home/mark.j.pernot/merge/archive_dir"
-                self.log_file="/home/mark.j.pernot/merge/log_dir/merge_repo.log"
-                self.to_line="Mark.J.Pernot@coe.ic.gov"
-                self.branch="master"
+                self.url = "git@gitlab.code.dicelab.net:JAC-IDM/"
+                self.work_dir = "/home/mark.j.pernot/merge/work_dir"
+                self.err_dir = "/home/mark.j.pernot/merge/error_dir"
+                self.archive_dir = "/home/mark.j.pernot/merge/archive_dir"
+                self.log_file = \
+                    "/home/mark.j.pernot/merge/log_dir/merge_repo.log"
+                self.to_line = "Mark.J.Pernot@coe.ic.gov"
+                self.branch = "master"
 
         self.cfg = CfgTest()
         self.subj = "Email_Subject"
