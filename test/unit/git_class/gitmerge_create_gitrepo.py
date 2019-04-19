ce56a9b3c7028137bf668d1bdaf2ffa9e0b26496
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-04-19 14:47:37 +0100

    Update test/unit/git_class/gitmerge_create_gitrepo.py

--- test/unit/git_class/gitmerge_create_gitrepo.py
+++ test/unit/git_class/gitmerge_create_gitrepo.py
@@ -70,6 +70,9 @@ class UnitTest(unittest.TestCase):
         self.branch = "Remote_branch"
         self.mod_branch = "Mod_branch"
 
+        self.gitr = git_class.GitMerge(self.repo_name, self.git_dir, self.url,
+                                       self.branch, self.mod_branch)
+
     @mock.patch("git_class.GitClass.create_repo")
     @mock.patch("git_class.GitClass.create_cmd")
     def test_create_gitrepo(self, mock_gitc, mock_gitr):
@@ -86,16 +89,15 @@ class UnitTest(unittest.TestCase):
         mock_gitc.return_value = True
         mock_gitr.return_value = True
 
-        gitr = git_class.GitMerge(self.repo_name, self.git_dir, self.url,
-                                  self.branch, self.mod_branch)
 
-        gitr.create_gitrepo()
+        self.gitr.create_gitrepo()
 
-        self.assertEqual((gitr.git_dir, gitr.repo_name, gitr.url,
-                          gitr.mod_branch, gitr.branch, gitr.remote_info,
-                          gitr.br_commit),
+        self.assertEqual((self.gitr.git_dir, self.gitr.repo_name,
+                          self.gitr.url, self.gitr.mod_branch,
+                          self.gitr.branch, self.gitr.remote_info,
+                          self.gitr.br_commit),
                          (self.git_dir, self.repo_name, self.url,
-                          self.mod_branch, gitr.branch, None, None))
+                          self.mod_branch, self.branch, None, None))
 
 
 if __name__ == "__main__":
