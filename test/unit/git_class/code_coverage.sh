9726ae0495ad681e1fc7bf2c3e0fc74f43c81945
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-04-22 17:07:40 +0100

    Update test/unit/git_class/code_coverage.sh

--- test/unit/git_class/code_coverage.sh
+++ test/unit/git_class/code_coverage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Unit test code coverage for merge_repo.py module.
+# Unit test code coverage for git_class.py module.
 # This will run the Python code coverage module against all unit test modules.
 # This will show the amount of code that was tested and which lines of code
 #	that was skipped during the test.
@@ -8,19 +8,12 @@ coverage erase
 
 echo ""
 echo "Running unit test modules in conjunction with coverage"
-coverage run -a --source=merge_repo test/unit/merge_repo/is_git_repo.py
-coverage run -a --source=merge_repo test/unit/merge_repo/load_cfg.py
-coverage run -a --source=merge_repo test/unit/merge_repo/merge.py
-coverage run -a --source=merge_repo test/unit/merge_repo/process_project.py
-coverage run -a --source=merge_repo test/unit/merge_repo/prepare_mail.py
-coverage run -a --source=merge_repo test/unit/merge_repo/move.py
-coverage run -a --source=merge_repo test/unit/merge_repo/post_process.py
-coverage run -a --source=merge_repo test/unit/merge_repo/post_check.py
-coverage run -a --source=merge_repo test/unit/merge_repo/merge_project.py
-coverage run -a --source=merge_repo test/unit/merge_repo/send_mail.py
-coverage run -a --source=merge_repo test/unit/merge_repo/help_message.py
-coverage run -a --source=merge_repo test/unit/merge_repo/run_program.py
-coverage run -a --source=merge_repo test/unit/merge_repo/main.py
+coverage run -a --source=git_class test/unit/git_class/gitmerge_init.py
+coverage run -a --source=git_class test/unit/git_class/gitmerge_create_gitrepo.py
+coverage run -a --source=git_class test/unit/git_class/gitmerge_set_remote.py
+coverage run -a --source=git_class test/unit/git_class/gitclass_init.py
+coverage run -a --source=git_class test/unit/git_class/gitclass_create_repo.py
+coverage run -a --source=git_class test/unit/git_class/gitclass_create_cmd.py
 
 echo ""
 echo "Producing code coverage report"
