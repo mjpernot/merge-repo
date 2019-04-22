879b24cab057756c696e8570aef446c2bba05f32
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-04-22 17:06:15 +0100

    Add test/unit/git_class/unit_test_run.sh

---
+++ test/unit/git_class/code_coverage.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Unit test code coverage for merge_repo.py module.
+# This will run the Python code coverage module against all unit test modules.
+# This will show the amount of code that was tested and which lines of code
+#	that was skipped during the test.
+
+coverage erase
+
+echo ""
+echo "Running unit test modules in conjunction with coverage"
+coverage run -a --source=merge_repo test/unit/merge_repo/is_git_repo.py
+coverage run -a --source=merge_repo test/unit/merge_repo/load_cfg.py
+coverage run -a --source=merge_repo test/unit/merge_repo/merge.py
+coverage run -a --source=merge_repo test/unit/merge_repo/process_project.py
+coverage run -a --source=merge_repo test/unit/merge_repo/prepare_mail.py
+coverage run -a --source=merge_repo test/unit/merge_repo/move.py
+coverage run -a --source=merge_repo test/unit/merge_repo/post_process.py
+coverage run -a --source=merge_repo test/unit/merge_repo/post_check.py
+coverage run -a --source=merge_repo test/unit/merge_repo/merge_project.py
+coverage run -a --source=merge_repo test/unit/merge_repo/send_mail.py
+coverage run -a --source=merge_repo test/unit/merge_repo/help_message.py
+coverage run -a --source=merge_repo test/unit/merge_repo/run_program.py
+coverage run -a --source=merge_repo test/unit/merge_repo/main.py
+
+echo ""
+echo "Producing code coverage report"
+coverage combine
+coverage report -m
---
+++ test/unit/git_class/unit_test_run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Unit testing program for the git_class.py module.
+# This will run all the units tests for this program.
+# Will need to run this from the base directory where the module file
+#   is located at.
+
+echo ""
+echo "Unit test:  gitmerge_init"
+test/unit/git_class/gitmerge_init.py
+
+echo ""
+echo "Unit test:  gitmerge_create_gitrepo"
+test/unit/git_class/gitmerge_create_gitrepo.py
+
+echo ""
+echo "Unit test:  gitmerge_set_remote"
+test/unit/git_class/gitmerge_set_remote.py
+
+echo ""
+echo "Unit test:  gitclass_init"
+test/unit/git_class/gitclass_init.py
+
+echo ""
+echo "Unit test:  gitclass_create_repo"
+test/unit/git_class/gitclass_create_repo.py
+
+echo ""
+echo "Unit test:  gitclass_create_cmd"
+test/unit/git_class/gitclass_create_cmd.py
+
