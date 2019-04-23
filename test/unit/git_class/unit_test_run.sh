76509900a201a019b2777eb5e2598824552add07
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-04-23 12:04:19 +0100

    Update test run files

--- test/unit/git_class/unit_test_run.sh
+++ test/unit/git_class/unit_test_run.sh
@@ -41,6 +41,10 @@ echo "Unit test:  gitmerge_git_fetch"
 test/unit/git_class/gitmerge_git_fetch.py
 
 echo ""
+echo "Unit test:  gitmerge_rename_br"
+test/unit/git_class/gitmerge_rename_br.py
+
+echo ""
 echo "Unit test:  gitclass_init"
 test/unit/git_class/gitclass_init.py
 
