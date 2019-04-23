c0c258059209052a616ce383769d89141db517f3
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-04-23 10:18:44 +0100

    Update test run files

--- test/unit/git_class/unit_test_run.sh
+++ test/unit/git_class/unit_test_run.sh
@@ -29,6 +29,10 @@ echo "Unit test:  gitmerge_process_untracked"
 test/unit/git_class/gitmerge_process_untracked.py
 
 echo ""
+echo "Unit test:  gitmerge_is_dirty"
+test/unit/git_class/gitmerge_is_dirty.py
+
+echo ""
 echo "Unit test:  gitclass_init"
 test/unit/git_class/gitclass_init.py
 
