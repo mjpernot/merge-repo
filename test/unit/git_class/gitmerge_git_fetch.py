382f1717b212996ce127d2e4c191aae54e03423e
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-04-23 11:40:29 +0100

    Update test/unit/git_class/gitmerge_git_fetch.py

--- test/unit/git_class/gitmerge_git_fetch.py
+++ test/unit/git_class/gitmerge_git_fetch.py
@@ -39,6 +39,20 @@ import version
 __version__ = version.__version__
 
 
+def fetch3():
+
+    """Function:  fetch3
+
+    Description:  Method stub holder for git.Repo.git.fetch().
+
+    Arguments:
+        None
+
+    """
+
+    raise git.exc.GitCommandError("git", 2, "stderr")
+
+
 def fetch2():
 
     """Function:  fetch2
@@ -50,7 +64,7 @@ def fetch2():
 
     """
 
-    raise git.exc.GitCommandError('git', 128)
+    raise git.exc.GitCommandError("git", 128, "stderr")
 
 
 def fetch():
@@ -79,7 +93,8 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Unit testing initilization.
-        test_git_fetch_false -> Test with exception raised from ls_remote call.
+        test_git_fetch_2 -> Test with raised exception - 2 status.
+        test_git_fetch_128 -> Test with raised exception - 128 status.
         test_git_fetch_true -> Test with successful ls_remote call.
 
     """
@@ -104,12 +119,28 @@ class UnitTest(unittest.TestCase):
         self.gitr = git_class.GitMerge(self.repo_name, self.git_dir, self.url,
                                        self.branch, self.mod_branch)
 
-    @unittest.skip("Not done")
-    def test_git_fetch_false(self):
+    def test_git_fetch_2(self):
 
-        """Function:  test_git_fetch_false
+        """Function:  test_git_fetch_2
 
-        Description:  Test with exception raised from ls_remote call.
+        Description:  Test with raised exception - 2 status.
+
+        Arguments:
+            None
+
+        """
+
+        GIT = collections.namedtuple('GIT', 'fetch')
+        self.gitr.gitcmd = GIT(fetch3)
+
+        status, msg = self.gitr.git_fetch()
+        self.assertEqual((status, msg["status"]), (False, 2))
+
+    def test_git_fetch_128(self):
+
+        """Function:  test_git_fetch_128
+
+        Description:  Test with raised exception - 128 status.
 
         Arguments:
             None
@@ -119,7 +150,8 @@ class UnitTest(unittest.TestCase):
         GIT = collections.namedtuple('GIT', 'fetch')
         self.gitr.gitcmd = GIT(fetch2)
 
-        self.assertFalse(self.gitr.git_fetch())
+        status, msg = self.gitr.git_fetch()
+        self.assertEqual((status, msg["status"]), (False, 128))
 
     def test_git_fetch_true(self):
 
