78ad14d1d3203f2df2183bffb6c431c4a57472d1
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-05-15 21:19:25 +0100

    Update test/unit/sonarqube_code_coverage.sh

--- test/unit/sonarqube_code_coverage.sh
+++ test/unit/sonarqube_code_coverage.sh
@@ -8,28 +8,6 @@ coverage erase
 
 echo ""
 echo "Running unit test modules in conjunction with coverage"
-coverage run -a --source=git_class test/unit/git_class/gitmerge_init.py
-coverage run -a --source=git_class test/unit/git_class/gitmerge_create_gitrepo.py
-coverage run -a --source=git_class test/unit/git_class/gitmerge_set_remote.py
-coverage run -a --source=git_class test/unit/git_class/gitmerge_is_remote.py
-coverage run -a --source=git_class test/unit/git_class/gitmerge_process_dirty.py
-coverage run -a --source=git_class test/unit/git_class/gitmerge_process_untracked.py
-coverage run -a --source=git_class test/unit/git_class/gitmerge_is_dirty.py
-coverage run -a --source=git_class test/unit/git_class/gitmerge_is_untracked.py
-coverage run -a --source=git_class test/unit/git_class/gitmerge_git_fetch.py
-coverage run -a --source=git_class test/unit/git_class/gitmerge_rename_br.py
-coverage run -a --source=git_class test/unit/git_class/gitmerge_git_co.py
-coverage run -a --source=git_class test/unit/git_class/gitmerge_priority_merge.py
-coverage run -a --source=git_class test/unit/git_class/gitmerge_git_pu.py
-coverage run -a --source=git_class test/unit/git_class/gitmerge_commits_diff.py
-coverage run -a --source=git_class test/unit/git_class/gitmerge_is_commits_ahead.py
-coverage run -a --source=git_class test/unit/git_class/gitmerge_is_commits_behind.py
-coverage run -a --source=git_class test/unit/git_class/gitmerge_is_remote_branch.py
-coverage run -a --source=git_class test/unit/git_class/gitmerge_get_dirty.py
-coverage run -a --source=git_class test/unit/git_class/gitmerge_get_untracked.py
-coverage run -a --source=git_class test/unit/git_class/gitclass_init.py
-coverage run -a --source=git_class test/unit/git_class/gitclass_create_repo.py
-coverage run -a --source=git_class test/unit/git_class/gitclass_create_cmd.py
 coverage run -a --source=merge_repo test/unit/merge_repo/merge.py
 coverage run -a --source=merge_repo test/unit/merge_repo/process_project.py
 coverage run -a --source=merge_repo test/unit/merge_repo/process_changes.py
