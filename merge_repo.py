a6d645b909a11851d5eeb6c0a06ebd3818f0bd76
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-04-01 13:45:29 +0100

    Add merge_repo.get_cmdline v0.3.2 - Returns the command line arguments

--- merge_repo.py
+++ merge_repo.py
@@ -834,8 +834,7 @@ def main(**kwargs):
 
     """
 
-    # sys.argv = kwargs.get("argv_list", sys.argv)
-    cmdline = kwargs.get("argv_list", get_cmdline(sys))
+    sys.argv = kwargs.get("argv_list", sys.argv)
 
     dir_chk_list = ["-d", "-p"]
     func_dict = {"-M": merge}
@@ -843,8 +842,7 @@ def main(**kwargs):
     opt_val_list = ["-c", "-d", "-p", "-r"]
 
     # Process argument list from command line.
-    # args_array = arg_parser.arg_parse2(sys.argv, opt_val_list)
-    args_array = arg_parser.arg_parse2(cmdline, opt_val_list)
+    args_array = arg_parser.arg_parse2(sys.argv, opt_val_list)
 
     if not gen_libs.help_func(args_array, __version__, help_message):
 
@@ -856,10 +854,8 @@ def main(**kwargs):
            and not arg_parser.arg_dir_chk_crt(args_array, dir_chk_list):
 
             try:
-                prog_lock = gen_class.ProgramLock(cmdline,
+                prog_lock = gen_class.ProgramLock(sys.argv,
                                                   args_array.get("-r", ""))
-                # prog_lock = gen_class.ProgramLock(sys.argv,
-                #                                   args_array.get("-r", ""))
                 run_program(args_array, func_dict)
                 del prog_lock
 
