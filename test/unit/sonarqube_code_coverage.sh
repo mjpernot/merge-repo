a2e0ff28150b30441a4b1d03bcb71ab0d69e0eb1
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-04-07 13:44:01 +0100

    Update test/unit/sonarqube_code_coverage.sh v0.3.2

--- test/unit/sonarqube_code_coverage.sh
+++ test/unit/sonarqube_code_coverage.sh
@@ -9,7 +9,6 @@ coverage erase
 echo ""
 echo "Running unit test modules in conjunction with coverage"
 coverage run -a --source=merge_repo test/unit/merge_repo/detach_head.py
-coverage run -a --source=merge_repo test/unit/merge_repo/get_cmdline.py
 coverage run -a --source=merge_repo test/unit/merge_repo/merge.py
 coverage run -a --source=merge_repo test/unit/merge_repo/_process_changes.py
 coverage run -a --source=merge_repo test/unit/merge_repo/process_project.py
